e787c9d06ef34ee6f9ef45a04d5d2afaf409627e
Robert Parker <rparker@pop-os.localdomain>
2021-03-30 10:12:50 -0500

    Add ingredients array to recipeSchema. Ingredients array has it's own ingredientSchema. Change updateNewRecipe to include list with ingredient objects. Create Edit.js with base div container.

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Ingredient = new Schema(
+    {
+        name: { type: String, required: true },
+        calories: {type: Number, required: true },
+        protein: Number,
+        totFat: Number,
+        satFat: Number,
+        unsatFat: Number,
+        carbs: Number,
+        fiber: Number,
+        sugar: Number,
+        sodium: Number,
+        chol: Number,
+        potas: Number,
+        quantity: { type: Number, required: true }
+    }
+);
+
 const Recipe = new Schema(
     {
         name: { type: String, required: true },
@@ -15,7 +33,8 @@ const Recipe = new Schema(
         sodium: Number,
         chol: Number,
         potas: Number,
-        servings: Number
+        servings: Number,
+        ingredients: [Ingredient]
     }
 );
 
